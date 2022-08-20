f188b09fb8e05fa2167a553d2b3d14a6372cb752
Brenda <waiyegobrenda99@gmail.com>
2022-08-20 22:54:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ function writeCards(people, event) {
   for (let i = 0; i < people.length; i++) {
     watu.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
   }
-
   return watu ;
 }
-
 function countDown(num) {
   
   while (num>=0){
